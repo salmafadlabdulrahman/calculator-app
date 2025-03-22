a99cc843a47f24217492491a5d2250b63dcef0aa
Salma <salmafadlabdulrahman@gmail.com>
2025-03-22 03:26:41 +0200

    add switch button

--- script.js
+++ script.js
@@ -14,13 +14,34 @@ const operations = [
   "7",
   "8",
   "9",
-  "Del"
+  "Del",
 ];
 
 const operationsContainer = document.getElementsByClassName(".operations");
-document.addEventListener("DOMContentLoaded", function() {
-    
-    operationsContainer.innerHTML = <p></p>
-    console.log(operations.map((operation) => operation))
-})
+let toggleInput = document.getElementsByTagName("input")[0];
+let toggleLabel = document.getElementsByClassName("toggle-label")[0];
+let toggleSwitch = document.getElementsByClassName("toggle-switch")[0];
 
+
+
+document.addEventListener("DOMContentLoaded", function () {});
+
+let curTheme = 1;
+toggleInput.addEventListener("change", function() {
+  toggleLabel.classList.remove("theme-one", "theme-two", "theme-three");
+  if (curTheme === 1) {
+    toggleLabel.classList.add("theme-two");
+    curTheme = 2
+  } else if (curTheme === 2) {
+    toggleLabel.classList.add("theme-three");
+    curTheme = 3
+  } else {
+    curTheme = 1;
+    toggleLabel.classList.add("theme-one");
+  }
+
+  
+
+  console.log(toggleLabel);
+  console.log(curTheme)
+})
